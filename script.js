9e86682e120f9dbc5963208be167aff2bf71be09
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-24 21:21:16 +0330

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => {console.log(json)
+    json.forEach(entry => {
+      const 
+      
+    });
+  
+  
+  
+  
+  
+  });
+
+
+
