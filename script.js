d9c053aa57ef8c1a128c291e71fd473eee4044b6
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-25 16:55:23 +0330

    Initial Commit

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ const renderBars = (data) => {
   const containerEl = document.querySelector(".days-column");
   containerEl.innerHTML = barsElements.join("");
 };
+// const pricerender = (data) => {
+//   const priceElement = data.amount.reduce((accumulator, currentValue) => {
+//     return `
+//     <div class="total-month">
+//      <div class="total-title">Total this month</div>
+//      <div class="total-price">$${accumulator + currentValue}</div>
+//     </div>`;
+//   }, 0);
+
+//   const totalEl = document.querySelector('.total-month')
+//   totalEl.innerHTML = priceElement
+// };
