79b8f96a9b6fcc2b0576ad430ad03cbe1528d0b9
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-25 13:27:55 +0330

    Initial Commit

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 "use strict";
 
-fetch("./data.json")
+fetch("data.json")
   .then((response) => response.json())
-  .then((json) => {console.log(json)
-    json.forEach(entry => {
-      const 
-      
+  .then((data) => {
+    console.log(data);
+    data.forEach((entry) => {
+      const dayEl = document.querySelector(`[day="${entry.day}"]`);
+      if (dayEl) {
+        statHeight = entry.amount * 2.86;
+        console.log(statHeight);
+        // dayEl.style.height = `${statHeight}px`;
+      }
     });
-  
-  
-  
-  
-  
   });
-
-
-
